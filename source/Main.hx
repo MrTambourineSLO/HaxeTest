ef5cd6a350dfec81267d70d44e674c9cc22b8063
MrTambourineSLO <saso.miocinovic@gmail.com>
2018-02-10 14:33:03 +0100

    Some imports

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,8 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+		// We set Game Width/Height here to 320 * 180
+		// in Project.xml we set it ot 640 * 360 which makes it 2X scaled @ 16:9 ratio
 		addChild(new FlxGame(320, 180, PlayState));
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,16 @@
 package;
 
 import flixel.FlxState;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState; 
+import flixel.text.FlxText; 
+import flixel.ui.FlxButton; 
+import flixel.math.FlxMath;
+// For creating game map
+import flixel.tile.FlxTilemap;
+// Contains some collision logic
+import flixel.FlxObject;
 
 class PlayState extends FlxState
 {
